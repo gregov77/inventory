3fe0ef168cb181c881910f480685369e5ee7ed8e
gregov77 <gregory.vieux@gmail.com>
2020-01-20 17:43:03 +0000

    Added forms for the searchItem view

--- app/forms.py
+++ app/forms.py
@@ -1,16 +1,45 @@
 from .models import Optics
-from flask_mongoengine.wtf import model_form
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, SelectField
+from wtforms import StringField, IntegerField, SubmitField, SelectField, HiddenField, FormField, FieldList
 from wtforms.validators import DataRequired
 
-class OpticsForm(FlaskForm):
+class AddedItemForm(FlaskForm):
+    '''
+        Generic form displayed to add item to database from 
+        the newItem page.
+    '''
+    part_number = StringField('Part number', validators=[DataRequired()])
+    quantity = IntegerField('Quantity',validators=[DataRequired()]) 
+    submit = SubmitField('Submit')   
+
+
+class SearchedItemForm(FlaskForm):
+    '''
+        Generic form for a single item returned from a search
+        in the searchItem page.
+
+    Note:
+        This class forms the building block of the class SearchedItemListForm. 
+    '''
+    _id = HiddenField('id', validators=[DataRequired()])
     part_number = StringField('Part number', validators=[DataRequired()])
     quantity = IntegerField('Quantity',validators=[DataRequired()])
+
+
+class SearchedItemListForm(FlaskForm):
+    '''
+        Form consisting of a list of SearchItemForm. This is the form
+        that is passed to the searchItem page to display the found
+        items.
+    '''
+    items = FieldList(FormField(SearchedItemForm))
     submit = SubmitField('Submit')
 
 
 class SearchForm(FlaskForm):
+    '''
+        Class defining the search form for the searchItem page.
+    '''
     searchField = SelectField(u'Search by:', choices=[('part_number', 'part number')])
     searchValue = StringField(validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, mongo
-from .forms import OpticsForm, SearchForm
+from .forms import AddedItemForm, SearchedItemForm, SearchedItemListForm, SearchForm
 from .models import Optics
 
 
@@ -12,22 +12,37 @@ def index():
 
 @app.route('/newItem', methods = ['GET', 'POST'])
 def newItem():
-    form = OpticsForm()
+    form = AddedItemForm()
     if form.validate_on_submit():
         optics = Optics(part_number=form.part_number.data, 
                         quantity=form.quantity.data)
         mongo.db.optics.insert_one(optics.__dict__)
         flash(f'Item added: {optics.__dict__}')
         return redirect('/newItem')
+    
     return render_template('newItem.html', title='Add item', form=form)
 
 
 @app.route('/searchItem', methods = ['GET', 'POST'])
 def searchItem():
     form = SearchForm()
-    results = None
+    items = SearchedItemListForm()
+    text_field=[]   
+
     if form.validate_on_submit():
+        
         searchfield = form.searchField.data
         searchvalue = form.searchValue.data
         results = mongo.db.optics.find({searchfield:searchvalue})
-    return render_template('searchItem.html', title='Search item', form=form, results=results)
+        
+        for result in results:
+            item = SearchedItemForm()
+            item._id.data = result['_id']
+            item.part_number.data = result['part_number']
+            text_field.append(result['part_number'])
+            item.quantity.data = result['quantity']
+            print(text_field)
+            items.items.append_entry(item)
+
+    return render_template('searchItem.html', title='Search item', 
+                           form=form, items=items, text_field=text_field)
