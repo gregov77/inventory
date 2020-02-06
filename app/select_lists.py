376779558bbabdbd44591ce7fae6a7c92ccaca18
gregov77 <gregory.vieux@gmail.com>
2020-02-06 17:37:14 +0000

    new commit

---
+++ app/select_lists.py
@@ -0,0 +1,14 @@
+optics_choices = [
+    ('Beamsplitters', 'Beamsplitters'),
+    ('Diffusers', 'Diffusers'),
+    ('Filters', 'Filters'),
+    ('Gratings', 'Gratings'),
+    ('Lenses', 'Lenses'),
+    ('Mirrors', 'Mirrors'),
+    ('Objective_lenses', 'Objective lenses'),
+    ('Polarizers', 'Polarizers'),
+    ('Prisms', 'Prisms'),
+    ('Reticles', 'Reticles'),
+    ('Windows', 'Windows')
+]
+
